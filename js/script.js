fc8338bb3557b81f2ed4a12b410782ae9d44e1b7
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-05-19 17:17:10 +0300

    Add CommandsInfluencer comments

--- js/script.js
+++ js/script.js
@@ -144,6 +144,7 @@ let CONFIG = {
    * The order and limit for each suggestion influencer. An "influencer" is
    * just a suggestion source. The following influencers are available:
    *
+   * - "Commands" suggestions come from CONFIG.commands
    * - "Default" suggestions come from CONFIG.defaultSuggestions
    * - "DuckDuckGo" suggestions come from the duck duck go search api
    * - "History" suggestions come from your previously entered queries
